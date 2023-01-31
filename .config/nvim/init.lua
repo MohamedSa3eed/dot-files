5da7b12b9b4b0338f7a72cca324b12d3909f6d0e
MohamedSa3eed <mohamed.sa3ed.faried@gmail.com>
2023-01-31 21:53:53 +0200

    add & configure Treesitter for neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -408,6 +408,32 @@ telescope.setup {
   },
 }
 
+--Treesitter
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
+
+configs.setup {
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    disable = { "" }, -- list of language that will be disabled
+    additional_vim_regex_highlighting = true,
+  },
+  indent = { enable = true, disable = { "yaml" } },
+  rainbow = {
+      enable = true,
+      -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+      max_file_lines = nil, -- Do not enable for files with more than n lines, int
+      -- colors = {}, -- table of hex strings
+      -- termcolors = {} -- table of colour name strings
+    }
+}
+
 -- Install your plugins here
 return packer.startup(function(use)
   -- My plugins here
@@ -441,6 +467,14 @@ return packer.startup(function(use)
   --Telescope
   use "nvim-telescope/telescope.nvim"
   use 'nvim-telescope/telescope-media-files.nvim'
+
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+   -- run = ":TSUpdate",
+  }
+  use "p00f/nvim-ts-rainbow"
+  --use "nvim-treesitter/playground"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
