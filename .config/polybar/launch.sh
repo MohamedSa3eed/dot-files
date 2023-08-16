47267d6e756d5eb913031835890abbd5c9ee9595
MohamedSa3eed <mohamed.sa3ed.faried@gmail.com>
2023-08-16 15:24:55 +0300

    cpu temp can be monitored now after reboot

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,12 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
+for i in /sys/devices/platform/coretemp.0/hwmon/hwmon*/temp1_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Core 0" ]; then
+        export HWMON_PATH="$i"
+    fi
+done
+
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar primary  2>&1 | tee -a /tmp/polybar1.log & disown
